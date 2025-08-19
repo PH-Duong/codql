1ceee2cf4a59e16c38aacbaf3286d9571ad00734
HeLi <145310853+PH-Duong@users.noreply.github.com>
2025-08-19 19:19:38 +0700

    Update Toasteroid.java

--- src/main/java/com/android/insecurebankv2/Toasteroid.java
+++ src/main/java/com/android/insecurebankv2/Toasteroid.java
@@ -12,14 +12,10 @@ public class Toasteroid {
     public static class STYLES {
         public static final int SUCCESS = 1;
         public static final int ERROR = 2;
+        public static final int WARNING = 3;
     }
 
-    public static class POSITION {
-        public static final int BOTTOM = Gravity.BOTTOM;
-        public static final int TOP = Gravity.TOP;
-    }
-
-    public static void show(Context context, String message, int style, int duration, int position) {
+    public static void show(Context context, String message, int style, int duration) {
         switch (style) {
             case STYLES.SUCCESS:
                 Toasty.success(context, message, duration, true).show();
@@ -27,9 +23,12 @@ public class Toasteroid {
             case STYLES.ERROR:
                 Toasty.error(context, message, duration, true).show();
                 break;
+            case STYLES.WARNING:
+                Toasty.warning(context, message, duration, true).show();
+                break;
             default:
                 Toast toast = Toast.makeText(context, message, duration);
-                toast.setGravity(position | Gravity.CENTER_HORIZONTAL, 0, 100);
+                toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 100);
                 toast.show();
         }
     }
